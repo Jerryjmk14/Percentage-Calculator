8e8f291ff24757211e336297de28b7bf42774db8
Jeremiah Egwa <jerriee232@gmail.com>
2024-06-19 15:53:18 +0100

    implemented percentage calculation to display the answer on the page

--- calculator.js
+++ calculator.js
@@ -6,11 +6,21 @@ var resultField = document.getElementById("resultField");
 
 var form = document.getElementById("xIsWhatPercentOfY");
 
-form.addEventListener("submit", function () {
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
   if (!numField1.value || !numField2.value) {
     alert("Please enter values in the text fields");
   } else {
     var x = parseFloat(numField1.value);
     var y = parseFloat(numField2.value);
+
+    var result = x / y;
+    var percent = result * 100;
+
+    resultField.innerText = "Answer: " + percent + "%";
+
+    numField1.value = "";
+    numField2.value = "";
   }
 });
